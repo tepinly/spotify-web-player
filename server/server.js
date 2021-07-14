ca896913ba8f83a7dd6d71b4e83d49acd3827eef
Youssef <youssef_a_fadel@hotmail.com>
2021-07-14 06:12:47 +0200

    spotify authorization

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: "http://localhost:3000",
+        clientId: "d278a33f5f394798861be3c96263064c",
+        clientSecret: "2174df3557ae43379184e4cd22a9138b",
+        refreshToken,
+    });
+
+    spotifyApi
+        .refreshAccessToken()
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            res.sendStatus(400);
+        });
+});
+
+app.post("/login", (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: "http://localhost:3000",
+        clientId: "d278a33f5f394798861be3c96263064c",
+        clientSecret: "2174df3557ae43379184e4cd22a9138b",
+    });
+
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then((data) => {
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in,
+            });
+        })
+        .catch(() => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+});
+
+app.listen(3001);
