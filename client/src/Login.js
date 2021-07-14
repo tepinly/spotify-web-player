44463c73792c4d9b963b942893b35e1a15bfccef
Youssef <youssef_a_fadel@hotmail.com>
2021-07-14 04:22:56 +0200

    changing 'login' to 'Login'

---
+++ client/src/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=d278a33f5f394798861be3c96263064c&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+
+export default function login() {
+    return <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <a className="btn btn-success btn-lg" href={ AUTH_URL }>Login with Spotify</a>
+    </Container>
+}
