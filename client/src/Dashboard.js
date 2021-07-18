21a37bd782f57d315569ea2f7837150d7d3f97fa
Youssef <youssef_a_fadel@hotmail.com>
2021-07-18 19:20:02 +0200

    adding frontend

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { useState } from "react";
 import useAuth from "./useAuth";
+import { Container, Form } from "react-bootstrap";
 
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code);
-    return <div>{code}</div>;
+    const [search, setSearch] = useState("");
+
+    return (
+        <Container
+            className="d-flex flex-column py-2"
+            style={{ height: "100vh" }}
+        >
+            <Form.Control
+                type="search"
+                placeholder="Search Songs/Artists"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+                Songs
+            </div>
+            <div>Bottom</div>
+        </Container>
+    );
 }
